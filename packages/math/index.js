c2227cebe1171191cfe2efe66f3cfb079a4e48dd
Bryce DesBrisay <bryce@desbrisay.com>
2018-10-11 15:14:44 -0600

    updated math package

--- packages/math/index.js
+++ packages/math/index.js
@@ -2,8 +2,25 @@
 const mathjs = require('mathjs');
 
 async function math(query) {
-  return mathjs.eval(query);
+  const data = mathjs.eval(query);
+
+  return `
+    <div class="mainCol mathContain">
+      <p>${query}</p>
+      <h1>${typeof data === 'number' && data}</h1>
+      ${(data && data.value) ? `<h1>${data.value}</h1>` : ``}
+    </div>
+    <style>
+      .mathContain {
+        padding: 0 15px;
+        box-sizing: border-box;
+      }
+    </style>
+  `;
 }
+
+// This line is for testing package with browserify bundle 
+window.math = math("2 + 2");
 
 async function trigger(query) {
   try {
