a368c46829005ff1ecdb8c71477168b5dcb2937b
Bryce DesBrisay <bryce@desbrisay.com>
2018-11-05 17:49:55 -0800

    piped new dictionary data into package

--- packages/dictionary/index.js
+++ packages/dictionary/index.js
@@ -1,9 +1,40 @@
 'use strict';
+const Dictionary = require("oxford-dictionary");
+  
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({path: `${__dirname}/../../.env`});
+}
 
 async function dictionary(query) {
-	const word = query.replace('define', '').trim();
-	const stuff = 'foo';
-	return `<span>${word}</span>`;
+	try {
+		const config = {
+			app_id : process.env.OXFORD_APP_ID,
+			app_key : process.env.OXFORD_APP_KEY,
+			source_lang : "en"
+		};
+		const dict = new Dictionary(config);
+		const line = query.replace('define', '').trim();
+		let results;
+
+		await dict.find(line).then(function(res) {
+			results = JSON.parse(JSON.stringify(res, null, 4));
+		}, 
+		function(err) {
+			console.log(err);
+		});
+
+		if (results) {
+			const provider = results.metadata.provider;
+			console.log(results.results)
+			const data = results.results[0];
+			const word = data.word;
+
+			return `<div>${word} ${provider}</div>`;
+		}
+	}
+	catch {
+
+	}
 }
 
 async function trigger(query) {
