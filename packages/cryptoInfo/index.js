7ed4acdf761a5f46830e82d8c0313e383d89af9e
David Keefe <david_keefe@hotmail.com>
2020-03-13 14:41:21 -0400

    Added a 'View on CMC' button to the cryptoInfo package

--- packages/cryptoInfo/index.js
+++ packages/cryptoInfo/index.js
@@ -13295,7 +13295,7 @@ async function cryptoInfo() {
                 ? `<div class="tag">${meta.category.toUpperCase()}</div>`
                 : ``
               }
-              ${(meta.tags !== null) 
+              ${(meta.tags !== null)
                 ? meta.tags.map((tag, index) => (
                     `<div
                       class="tag"
@@ -13309,7 +13309,7 @@ async function cryptoInfo() {
               }
             </div>
             <div class="priceRow">
-              ${quote.price 
+              ${quote.price
                 ? `<div class="priceItem">
                     <p class="priceLabel">Price</p>
                     <h1
@@ -13365,6 +13365,10 @@ async function cryptoInfo() {
                   <span>Website</span>
                 </a>`
               ))}
+              <a href="https://coinmarketcap.com/currencies/${meta.slug}" class="linkItem">
+                <svg class="svg-inline--fa fa-w-16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 76.52 77.67"><path d="m66.54 46.41a4.09 4.09 0 0 1 -4.17.28c-1.54-.87-2.37-2.91-2.37-5.69v-8.52c0-4.09-1.62-7-4.33-7.79-4.58-1.34-8 4.27-9.32 6.38l-8.1 13.11v-16c-.09-3.69-1.29-5.9-3.56-6.56-1.5-.44-3.75-.26-5.94 3.08l-18.11 29.07a32 32 0 0 1 -3.64-14.94c0-17.52 14-31.77 31.25-31.77s31.3 14.25 31.3 31.77v.09s0 .06 0 .09c.17 3.39-.93 6.09-3 7.4zm10-7.57v-.17c-.14-21.35-17.26-38.67-38.29-38.67s-38.25 17.42-38.25 38.83 17.16 38.84 38.25 38.84a37.81 37.81 0 0 0 26-10.36 3.56 3.56 0 0 0 .18-5 3.43 3.43 0 0 0 -4.86-.23 30.93 30.93 0 0 1 -44.57-2.08l16.3-26.2v12.09c0 5.81 2.25 7.69 4.14 8.24s4.78.17 7.81-4.75l9-14.57c.28-.47.55-.87.79-1.22v7.41c0 5.43 2.18 9.77 6 11.91a11 11 0 0 0 11.21-.45c4.2-2.73 6.49-7.67 6.25-13.62z" fill="#17181b"/></svg>
+                <span>View on CMC</span>
+              </a>
               ${meta.urls.source_code.map((item, index) => (
                 `<a href="${item}" key="${index}" class="linkItem">
                   <i class="fa fa-code"></i>
@@ -13539,9 +13543,9 @@ async function cryptoInfo() {
         .linkItem {
           display: inline-flex;
           flex-direction: row;
-          padding: 8px 15px 8px 10px;
+          padding: 8px 10px;
           background-color: #ddd;
-          margin: 5px 10px 5px 0;
+          margin: 5px 5px 5px 0;
           border-radius: 5px;
           text-decoration: none;
         }
@@ -13549,7 +13553,7 @@ async function cryptoInfo() {
           background-color: #ccc;
         }
         .linkItem span {
-          margin-left: 10px;
+          margin-left: 8px;
           font-size: 14px;
         }
         .explorer {
