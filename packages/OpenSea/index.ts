4216d4e6b0c1614d874fa4773bc867ba1d35b632
Awa Dieudonne <dieudonneawa7@gmail.com>
2021-08-23 18:08:04 +0100

    feature: show all query matches

--- packages/OpenSea/index.ts
+++ packages/OpenSea/index.ts
@@ -1,12 +1,14 @@
 'use strict';
 
-import { IAsset } from "./types";
+import { IAsset } from './types';
 
 // @ts-ignore
 const fetch = require('node-fetch');
 
 const loadAssets = async (): Promise<Array<IAsset>> => {
-  const response = await fetch('https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=50');
+  const response = await fetch(
+    'https://api.opensea.io/api/v1/assets?order_by=num_sales&order_direction=desc&offset=0&limit=50'
+  );
   const data = await response.text();
   const { assets } = JSON.parse(data);
   return assets;
@@ -16,46 +18,56 @@ async function OpenSea(query: string): Promise<string> {
   const q = query.toLowerCase().split(' ');
   const assets = await loadAssets();
   // Search by asset name
-  let searchedAsset =
+  let searchedAssets =
     (assets &&
-      assets.find((asset) => {
+      assets.filter((asset) => {
         if (asset.name && q.every((el) => asset.name.toLowerCase().indexOf(el) > -1)) {
           return true;
         } else if (asset.traits.some((trait) => trait.trait_type)) {
         }
       })) ||
-    {} as IAsset;
+    ([] as IAsset[]);
 
   // Search asset by asset_contract name
-  if (!searchedAsset) {
-    searchedAsset =
-      assets.find(
+  if (!searchedAssets) {
+    searchedAssets =
+      assets.filter(
         (asset) =>
           asset.asset_contract.name &&
           q.every((el) => asset.asset_contract.name.toLowerCase().indexOf(el) > -1)
-      ) || {} as IAsset;
+      ) || ([] as IAsset[]);
   }
 
-  console.log(searchedAsset);
+  const searchedAsset = searchedAssets[0];
+  searchedAssets.splice(0, 1);
 
-  return `<div class="min-vh-screen w-full flex flex-col items-start bg-cultured">
-    <div style="width: 1000px; height: 450px; padding: 2rem;" class="flex bg-light-white justify-between">
+  console.log(searchedAssets.length);
+
+  return `<div class="w-full flex flex-col items-start bg-cultured">
+    <div style="max-width: 1100px; min-height: 450px; padding: 2rem;" class="flex bg-light-white justify-between">
       <div style="width: 40%; padding: 1rem; overflow: hidden;" class="border flex justify-center items-center bg-white">
-        <img width="400px" height="380px" src="${searchedAsset.image_url}" alt="${searchedAsset.name}" />
+        <img width="400px" height="380px" src="${searchedAsset ? searchedAsset.image_url : ''}" alt="${
+    searchedAsset ? searchedAsset.name : ''
+  }" />
       </div>
       <div class="flex flex-col items-start" style="width: 55%; padding: 1rem; flex: 1;">
-        <h3>${searchedAsset.name}</h3>
-        <a href="${
-          searchedAsset.external_link || searchedAsset.permalink
+        <h3 class="fw-bold">${searchedAsset ? searchedAsset.name : ''}</h3>
+        <a style="margin-bottom: 1rem;" href="${
+          searchedAsset ? searchedAsset.external_link || searchedAsset.permalink : ''
         }" class="text-grey-web">View on OpenSea</a>
+        <span class="text-grey-web--dark">${searchedAsset ? searchedAsset.description : ''}</span>
         <span class="flex items-center text-grey-web" style="margin-top: 1rem;">
           <span style="height: 25px; width: 25px; overflow: hidden; border-radius: 50%; margin-right: 0.5rem;" class="flex justify-center items-center">
-            <img src="${searchedAsset.owner ? searchedAsset.owner.profile_img_url : ''}" width="40px" />
+            <img src="${
+              searchedAsset && searchedAsset.owner ? searchedAsset.owner.profile_img_url : ''
+            }" width="40px" />
           </span>
           Owned by &nbsp; <a class="text-bluetiful" href="${
-            searchedAsset.owner ? searchedAsset.owner.profile_img_url : ''
+            searchedAsset && searchedAsset.owner ? searchedAsset.owner.profile_img_url : ''
           }">${
-    searchedAsset.owner && searchedAsset.owner.user ? searchedAsset.owner.user.username : 'N/A'
+    searchedAsset && searchedAsset.owner && searchedAsset.owner.user
+      ? searchedAsset.owner.user.username
+      : 'N/A'
   }</a>
         </span>
         <div style="margin-top: 0.6rem;" class="border h-full w-full">
@@ -75,8 +87,8 @@ async function OpenSea(query: string): Promise<string> {
               0.003
               </h1>
             </span>
-            <a href="${searchedAsset.permalink}">
-              <button class="btn-primary cursor-pointer flex items-center rounded" style="border: 0; padding: 0.8rem 2.5rem; margin-bottom: 1rem;">
+            <a href="${searchedAsset ? searchedAsset.permalink : ''}">
+              <button class="btn--primary cursor-pointer flex items-center rounded" style="border: 0; padding: 0.8rem 2.5rem; margin-bottom: 1rem;">
                 BUY THIS ITEM
                 <span style="margin-left: 0.5rem;" class="material-icons">
                   keyboard_arrow_right
@@ -95,14 +107,37 @@ async function OpenSea(query: string): Promise<string> {
         </div>
       </div>
     </div>
+
+    <div style="width: 100%;" class="p-4 my-5 border rounded flex container w-100 bg-cultured">
+      <div class="row w-100">
+        ${
+          searchedAssets
+            ? searchedAssets
+                .map(
+                  (asset) => `
+                <div class="p-2 col-md-3">
+                  <div class="border bg-white p-2 d-flex flex-col">
+                    <img src="${asset.image_thumbnail_url}" width="100%" class="border" />
+                    <small class="text-grey-web mt-2">${asset.collection.name}</small>
+                    <span class="mb-3 mt-1 fw-bolder">${asset.name}</span>
+                  </div>
+                </div>
+                `
+                )
+                .join('')
+            : ''
+        }
+      </div>
+    </div>
   </div>`;
 }
 
 async function trigger(query: string): Promise<boolean> {
   const assets = await loadAssets();
-  // console.log(assets);
   const q = query.toLowerCase().split(' ');
-  if (assets.some((asset) => asset.name && q.some((el: string) => asset.name.toLowerCase().indexOf(el) > -1))) {
+  if (
+    assets.some((asset) => asset.name && q.some((el: string) => asset.name.toLowerCase().indexOf(el) > -1))
+  ) {
     return true;
   }
   let filteredAssets = assets.filter(
