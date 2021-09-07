f829b019f69f732d8f347bdc30848e75749445a7
Awa Dieudonne <dieudonneawa7@gmail.com>
2021-09-07 23:07:57 +0100

    feature(OpenSea): JavaScript version for open sea

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 "use strict";
 require("dotenv").config();
 
+const { promisify } = require("util");
+const fs = require("fs");
+
+const express = require("express");
+
 const fs = require("fs");
 const express = require("express");
 const packageObject = require(`${__dirname}/../index`);
@@ -38,7 +43,41 @@ const addRoutesInterval = setInterval(() => {
 }, 200);
 
 app.get("/", async (req, res) => {
-    res.render("index", { packageObject });
+  res.render("index", { packageObject });
+  const commonCss = await readFileAsync(`${__dirname}/../common.css`);
+  const packageKey = req.query.packageKey;
+  const query = req.query.query;
+  console.log(packageObject);
+  const trigger = await packageObject[packageKey].trigger(query);
+  if (packageKey in packageObject && trigger) {
+    const result = await packageObject[packageKey][packageKey](query);
+    res.send(`
+      <head>
+        <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+        <link href="https://fonts.googleapis.com/css2?family=Material+Icons"
+      rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+      </head>
+      <body>
+        <div class="answerInner">${result}</div>
+        <style type="text/css">
+          ${commonCss}
+          body {
+            margin: 0;
+            padding: 10px 0;
+            background-color: #f2f2f2;
+            font-family: Lato,Helvetica,sans-serif;
+            letter-spacing: .1px;
+          }
+        </style>
+      </body>
+    `);
+  } else {
+    res.status("400");
+    res.send(
+      "Bad request, package key does not exist or package was not triggered for specified query"
+    );
+  }
 });
 
 app.listen(PORT, () => console.log(`Server listening on: ${PORT}`));
