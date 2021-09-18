916109aafd79862feb79bf532f872a86500e9350
Awa Dieudonne <dieudonneawa7@gmail.com>
2021-09-18 19:14:35 +0100

    refactor: rewrite styles using classess

--- packages/OpenSea/index.js
+++ packages/OpenSea/index.js
@@ -3,6 +3,14 @@
 // @ts-ignore
 const fetch = require("node-fetch");
 
+// Used for accessing nested object properties and to avoid using multiple `&&`.
+const getNestedObject = (nestedObj, objPropsArr = []) => {
+  return objPropsArr.reduce(
+    (obj, key) => (obj && obj[key] !== "undefined" ? obj[key] : undefined),
+    nestedObj
+  );
+};
+
 const truncate = (fullStr, strLen, separator = "...") => {
   if (fullStr.length <= strLen) return fullStr;
 
@@ -40,107 +48,294 @@ const loadAssets = async (query) => {
 async function openSea(query) {
   const assets = await loadAssets(query);
 
-  const firstAsset =
-    assets &&
-    assets.find((asset) => {
-      return (
-        asset &&
-        ((asset.owner && asset.owner.address === query) ||
-          (asset.asset_contract && asset.asset_contract.address === query))
-      );
-    });
+  const mainAsset = assets &&
+    assets.find((asset) =>
+        getNestedObject(asset, ["owner", "address"]) === query ||
+        getNestedObject(asset, ["asset_contract", "address"]) === query
+    );
 
   return `
-  <div style="min-width: 90vw;" class="w-full flex flex-column flex-md-row flex-md-nowrap items-start bg-cultured">
-    <div style="max-width: 900px; min-height: 450px; padding: 2rem 1rem;" class="flex bg-light-white justify-between items-start me-3">
-      <div style="width: 40%; padding: 0 0; overflow: hidden;" class="border rounded flex justify-center items-start bg-white">
-        <img width="400px" height="380px" src="${
-          firstAsset
-            ? firstAsset.image_url ||
-              firstAsset.image_thumbnail_url ||
-              firstAsset.image_preview_url
-            : ""
-        }" alt="${firstAsset ? firstAsset.name || "N/A" : ""}" />
+  <style>
+    :root {
+      --cultured: #f3f4f6;
+      --white: #ffffff;
+      --light-white: #fbfbfb;
+      --gray-web: #7C7C7C;
+      --gray-web-dark: #555555;
+      --primary: #359ed8;
+      --bluetiful: #0066FF;
+      --border: 1px solid #e5e5e5;
+    }
+
+    .text-grey-web { color: var(--gray-web); }
+    .text-grey-web--dark { color: var(--gray-web-dark); }
+    .text-bluetiful { color: var(--bluetiful); }
+
+    .Package-wrapper {
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      align-items: flex-start;
+      background: var(--cultured);
+    }
+
+    .Package-wrapper .MainAsset {
+      max-width: 750px;
+      min-height: 450px;
+      padding: 2rem 1rem;
+      display: flex;
+      justify-content: space-between;
+      align-items: flex-start;
+      margin-right: 2rem;
+      background: var(--white);
+    }
+
+    .MainAsset .MainAsset--imgWrapper {
+      width: 40%;
+      padding: 0 0;
+      display: flex;
+      align-items: start;
+      justify-content: center;
+      overflow: hidden;
+      border-radius: 4px;
+      border: var(--border);
+      background: var(--white);
+    }
+
+    .MainAsset .MainAsset--content {
+      display: flex;
+      align-items: start;
+      flex-direction: column;
+      padding: 1rem;
+      padding-right: 0;
+      width: 55%;
+      flex: 1;
+    }
+
+    .MainAsset .MainAsset--name {
+      font-weight: bold;
+      font-size: 1.2rem;
+    }
+
+    .MainAsset .MainAsset--viewOnOpensea {
+      margin-bottom: 1rem;
+      margin-top: 0.3rem;
+    }
+
+    .MainAsset .MainAsset--ownerInfo {
+      display: flex;
+      align-items: center;
+      margin-top: 1rem;
+    }
+
+    .MainAsset .MainAsset--ownerInfo--profile {
+      height: 25px;
+      width: 25px;
+      overflow: hidden;
+      border-radius: 50%;
+      margin-right: 0.5rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    .MainAsset .MainAsset--buttonWrapper {
+      padding: 1rem 0;
+      flex: 1;
+    }
+
+    .MainAsset .MainAsset--buttonWrapper .MainAsset--button {
+      background: var(--primary);
+      color: var(--white);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border: 0;
+      padding: 0.8rem 2.5rem;
+      margin-bottom: 1rem;
+      border-radius: .25rem!important;
+    }
+
+    .OtherAssets {
+      max-width: 500px;
+      border-radius: .25rem!important;
+      background: var(--cultured);
+      margin: 0;
+      display: flex;
+      justify-content: space-between;
+      flex-flow: row wrap;
+    }
+
+    .OtherAssets .OtherAssets-cardWrapper {
+      padding: 0.4rem;
+      width: 47%;
+      min-width: 200px;
+    }
+
+    .OtherAssets .OtherAssets--card {
+       border: var(--border);
+       background: var(--white);
+       padding: 0.5rem;
+       display: flex;
+       flex-flow: column nowrap;
+       border-radius: .25rem!important;
+    }
+
+    .OtherAssets .OtherAssets--card img {
+       border: var(--border);
+    }
+
+    .OtherAssets .OtherAssets--assetCaption {
+      margin-top: 0.5rem;
+    }
+
+    .OtherAssets .OtherAssets--assetName {
+      margin-top: 0.2rem;
+      font-weight: 600;
+      cursor: pointer;
+      margin-bottom: 0.75rem;
+    }
+
+    @media screen and (max-width: 839px) {
+      .Package-wrapper {
+        flex-direction: column;
+      }
+
+      .Package-wrapper .MainAsset {
+        margin-right: 0;
+      }
+
+      .Package-wrapper .OtherAssets {
+        width: 100%;
+        justify-content: flex-start;
+        max-width: 100%;
+      }
+
+      .Package-wrapper .OtherAssets .OtherAssets-cardWrapper {
+        width: 40%;
+        margin: 0 0.5rem;
+      }
+
+      .Package-wrapper .OtherAssets .OtherAssets-cardWrapper .OtherAssets--card {
+        width: 100%;
+      }
+    }
+  </style>
+
+  <div class="Package-wrapper">
+    <div class="MainAsset">
+      <div class="MainAsset--imgWrapper">
+        <img
+          width="400px"
+          height="380px"
+          src="${
+            mainAsset
+              ? mainAsset.image_url ||
+                mainAsset.image_thumbnail_url ||
+                mainAsset.image_preview_url
+              : ""
+          }"
+          alt="${mainAsset ? mainAsset.name || "N/A" : ""}"
+        />
       </div>
-      <div class="flex flex-col items-start ps-3 py-3 pr-0" style="width: 55%; flex: 1;">
-        <strong class="fw-bold fs-5">${
-          firstAsset ? firstAsset.name || "NA" : ""
+
+      <div class="MainAsset--content">
+        <strong class="MainAsset--name">${
+          mainAsset ? mainAsset.name || "NA" : ""
         }</strong>
-        <a style="margin-bottom: 1rem;" href="${
-          firstAsset ? firstAsset.external_link || firstAsset.permalink : ""
-        }" class="text-grey-web">View on openSea</a>
-        <span class="text-grey-web--dark">${
-          firstAsset
-            ? firstAsset.description ||
-              firstAsset.name ||
-              (firstAsset.collection && firstAsset.collection.description)
-            : ""
-        }</span>
-        <span class="flex items-center text-grey-web" style="margin-top: 1rem;">
-          <span style="height: 25px; width: 25px; overflow: hidden; border-radius: 50%; margin-right: 0.5rem;" class="flex justify-center items-center">
-            <img src="${
-              firstAsset && firstAsset.owner
-                ? firstAsset.owner.profile_img_url
-                : ""
-            }" width="40px" />
-          </span>
-          Owned by &nbsp; <a class="text-bluetiful" href="${
-            firstAsset ? firstAsset.profile_img_url | "#" : ""
-          }">${
-    firstAsset && firstAsset.owner && firstAsset.owner.user
-      ? firstAsset.owner
-        ? truncate(firstAsset.owner.address, 12)
-        : ""
-      : "N/A"
-  }</a>
+
+        <a
+          href="${
+            mainAsset ? mainAsset.external_link || mainAsset.permalink : ""
+          }"
+          class="MainAsset--viewOnOpensea text-grey-web"
+        >
+          <small>View on openSea</small>
+        </a>
+
+        <span class="text-grey-web--dark">
+          ${
+            mainAsset
+              ? mainAsset.description ||
+                mainAsset.name ||
+                getNestedObject(mainAsset, ["collection", "description"])
+              : ""
+          }
         </span>
-        <div style="margin-top: 0.6rem;" class="h-full w-full">
-          <div style="padding: 1rem 0; flex: 1;" class="flex flex-col justify-start items-start">
-            <a href="${
-              firstAsset ? firstAsset.permalink || firstAsset.external_link : ""
-            }">
-              <button class="btn--primary cursor-pointer flex items-center rounded" style="border: 0; padding: 0.8rem 2.5rem; margin-bottom: 1rem;">
-                VIEW THIS ITEM
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/></svg>
-              </button>
-            </a>
-          </div>
+
+        <div class="MainAsset--ownerInfo text-grey-web">
+          <span class="MainAsset--ownerInfo--profile">
+            <img
+              src="${
+                getNestedObject(mainAsset, ["owner"])
+                  ? mainAsset.owner.profile_img_url
+                  : ""
+              }"
+              width="40px"
+            />
+          </span>
+          Owned by &nbsp;
+          <a
+            class="text-bluetiful"
+            href="${mainAsset ? mainAsset.profile_img_url | "#" : ""}"
+          >
+            ${
+              getNestedObject(mainAsset, ["owner", "user"])
+                ? mainAsset.owner
+                  ? truncate(mainAsset.owner.address, 12)
+                  : ""
+                : "N/A"
+            }
+          </a>
+        </div>
+        <div class="MainAsset--buttonWrapper">
+          <a href="${
+            mainAsset ? mainAsset.permalink || mainAsset.external_link : ""
+          }">
+            <button class="MainAsset--button">
+              VIEW THIS ITEM
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/></svg>
+            </button>
+          </a>
         </div>
       </div>
     </div>
 
-    <div style="max-width: 500px;" class="mx-2 rounded flex flex-column container-fluid bg-cultured">
-      <div class="row w-100">
-        ${
-          assets
-            ? assets
-                .filter((asset) => firstAsset.id !== asset.id)
-                .filter((_asset, i) => i < 4)
-                .map(
-                  (asset) => `
-                <div class="p-2 col-md-6">
-                  <div class="border rounded bg-white p-2 d-flex flex-col">
-                    <img src="${
-                      asset.image_thumbnail_url ||
-                      asset.image_preview_url ||
-                      asset.image_url
-                    }" width="100%" height="170px" class="border" />
-                    <small class="text-grey-web mt-2">${
+    <div class="OtherAssets">
+      ${
+        assets
+          ? assets
+              .filter((asset) => mainAsset.id !== asset.id)
+              .filter((_asset, i) => i < 4)
+              .map(
+                (asset) => `
+                <div class="OtherAssets-cardWrapper">
+                  <div class="OtherAssets--card">
+                    <img
+                      src="${
+                        asset.image_thumbnail_url ||
+                        asset.image_preview_url ||
+                        asset.image_url
+                      }"
+                      width="100%"
+                      height="170px"
+                    />
+                    <small class="OtherAssets--assetCaption text-grey-web">${
                       asset.collection.name
                     }</small>
-                    <a href="${
-                      asset.external_link || asset.permalink
-                    }" class="mb-3 mt-1 fw-bolder cursor-pointer">${
-                    asset.name || asset.collection.name || "NA"
-                  }</a>
+                    <a
+                      href="${asset.external_link || asset.permalink}"
+                      class="OtherAssets--assetName"
+                    >
+                      ${asset.name || asset.collection.name || "NA"}
+                    </a>
                   </div>
                 </div>
                 `
-                )
-                .join("")
-            : ""
-        }
-      </div>
+              )
+              .join("")
+          : ""
+      }
     </div>
   </div>`;
 }
