0d183a91941fe822493ff0ed222b1e6f720baea2
Jejo <jejopl@o2.pl>
2021-10-07 23:26:16 +0200

    fixed some edge cases, moved js to <script> tags

--- packages/math/index.js
+++ packages/math/index.js
@@ -5,69 +5,109 @@ async function math(query) {
   const data = mathjs.evaluate(query);
 
   return `
-     
 <div class="mainCol mathContain">
-      <form name="calculator">
+  <form name="">
    <table>
       <tr>
          <td colspan="4">
             <input  type="text" name="resultDisplay" value=${
               typeof data === "number" ? data : ``
             } id="resultDisplay" disabled>
-            
-   
          </td>
       </tr>
       <tr>
-            <td><input type="button" name="one" value="1" onclick="calculator.resultDisplay.value += '1'"></td>
-            <td><input type="button" name="two" value="2" onclick="calculator.resultDisplay.value += '2'"></td>
-            <td><input type="button" name="three" value="3" onclick="calculator.resultDisplay.value += '3'"></td>
-            <td><input type="button" class="operator" name="plus" value="+" onclick="calculator.resultDisplay.value += '+'"></td>
+            <td><input type="button" name="one" value="1" onclick="addValue('1')"></td>
+            <td><input type="button" name="two" value="2" onclick="addValue('2')"></td>
+            <td><input type="button" name="three" value="3" onclick="addValue('3')"></td>
+            <td><input style="margin-right:14px;" type="button" class="operator" name="plus" value="+" onclick="addValue('+')"></td>
      </tr>
      <tr>
-            <td><input type="button" name="four" value="4" onclick="calculator.resultDisplay.value += '4'"></td>
-            <td><input type="button" name="five" value="5" onclick="calculator.resultDisplay.value += '5'"></td>
-            <td><input type="button" name="six" value="6" onclick="calculator.resultDisplay.value += '6'"></td>
-            <td><input type="button" class="operator" name="minus" value="-" onclick="calculator.resultDisplay.value += '-'"></td>
+            <td><input type="button" name="four" value="4" onclick="addValue('4')"></td>
+            <td><input type="button" name="five" value="5" onclick="addValue('5')"></td>
+            <td><input type="button" name="six" value="6" onclick="addValue('6')"></td>
+            <td><input type="button" class="operator" name="minus" value="-" onclick="addValue('-')"></td>
      </tr>
      <tr>
-            <td><input type="button" name="seven" value="7" onclick="calculator.resultDisplay.value += '7'"></td>
-            <td><input type="button" name="eight" value="8" onclick="calculator.resultDisplay.value += '8'"></td>
-            <td><input type="button" name="nine" value="9" onclick="calculator.resultDisplay.value += '9'"></td>
-            <td><input type="button" class="operator" name="times" value="x" onclick="calculator.resultDisplay.value += '*'"></td>
+            <td><input type="button" name="seven" value="7" onclick="addValue('7')"></td>
+            <td><input type="button" name="eight" value="8" onclick="addValue('8')"></td>
+            <td><input type="button" name="nine" value="9" onclick="addValue('9')"></td>
+            <td><input type="button" class="operator" name="times" value="x" onclick="addValue('*')"></td>
      </tr>
      <tr>
-            <td><input type="button" id="clear" name="clear" value="c" onclick="calculator.resultDisplay.value = ''"></td>
-            <td><input type="button" name="zero" value="0" onclick="calculator.resultDisplay.value += '0'"></td>
-            <td><input type="button" name="doit" value="=" onclick="calculator.resultDisplay.value = eval(calculator.resultDisplay.value)"></td>
-            <td><input type="button" class="operator" name="div" value="/" onclick="calculator.resultDisplay.value += '/'"></td>
-      
+            <td><input type="button" id="clear" name="clear" value="C" onclick="calculator.resultDisplay.value = '0'"></td>
+            <td><input type="button" name="zero" value="0" onclick="addValue('0')"></td>
+            <td><input type="button" name="doit" value="=" onclick="calculator.resultDisplay.value = summary(calculator.resultDisplay.value)"></td>
+            <td><input type="button" class="operator" name="div" value="/" onclick="addValue('/')"></td>
       </tr>
    </table>
-</form>
+  </form>
 </div>
+
+<script>
+const addValue = (addedValue) => {
+  const getValue = (value) => {
+    //* check if the value is an operator or a number
+    const isOperator = (value = "") => {
+      if (!value.match(/[0-9]/)) return true;
+      return false;
+    }
+    let currentValue = calculator.resultDisplay.value;
+    const previousValue = currentValue && currentValue.length ? currentValue[currentValue.length-1] : "";
+    const newValue = currentValue + addedValue;
+    //* check if we have two operators one by one, but allow (*- and /-)
+    const twoOperators = (isOperator(value) && isOperator(previousValue)) && ((!newValue.includes("*-") && !newValue.includes("/-")) || newValue.includes("--"));
+    //* reset calculator when we will start with 0 value
+    if (currentValue === "0") currentValue = "";
+    if (value && value.length) {
+      //* do nothing if the previous value vas an operator, or if the first value is an operator (except '-')
+      if (twoOperators || (currentValue.length === 1 && currentValue[0] !== "-" && isOperator(currentValue[0]))) {
+        return currentValue;
+      }
+      return newValue;
+    }
+    return currentValue;
+  }
+
+  calculator.resultDisplay.value = getValue(addedValue);
+};
+const summary = (value) => {
+  //* fix the case where we have operator on the last place, ie eval("50*")
+  try {
+    eval(value)
+  } catch(err) {
+    return value;
+  }
+  //* check if number has more than 12 digts after decimal place
+  const test = eval(value) * Math.pow(10, 12)
+  if (test % 10 !== 0) {
+    return eval(value).toFixed(12);
+  }
+  return eval(value);
+}
+</script>
  
 
-    <style>
+<style>
 .dark .mathContain {
-        color:#d1d5db;
-      }
-      .mathContain {
-        padding: 0 15px;
-        box-sizing: border-box;
-      }
+  color:#d1d5db;
+}
+.mathContain {
+  padding: 0 15px;
+  box-sizing: border-box;
+}
       
-table {
-   background-color: #1e1e1e;
+.mainCol table {
+  background-color: #1e1e1e;
   width: 295px;
   max-width: 295px;
   height: 325px;
   text-align: center;
   border-radius: 8px;
   padding-right: 10px;
+  margin-bottom: 20px;
 }
 
-input {
+.mainCol input {
   outline: 0;
   position: relative;
   left: 5px;
@@ -85,7 +125,7 @@ input {
   margin-bottom: 15px;
 }
 
-input:hover {
+.mainCol input:hover {
     color: #495069;
   background-color: #fff;
   border-radius: 4px;
@@ -95,7 +135,7 @@ input:hover {
   font-size: 20px;
   }
 
-input:active {
+.mainCol input:active {
   top: 4px;
   border: 0 solid #000;
   color: #495069;
@@ -109,39 +149,40 @@ input:active {
   box-shadow: none;
 }
 
-#resultDisplay {
+.mainCol #resultDisplay {
   width: 265px;
   max-width: 270px;
   font-size: 26px;
   text-align: right;
   background-color:  #d1d5db;
   float: left;
+  padding-right: 10px;
 }
 
-.operator {
+.mainCol .operator {
   background-color: #cee9ef;
   position: relative;
 }
 
-.operator:hover {
+.mainCol .operator:hover {
 
   color: #495069;
   
 }
 
-.operator:active {
+.mainCol .operator:active {
   top: 4px;
   box-shadow: none;
 }
 
-#clear {
+.mainCol #clear {
   float: left;
   position: relative;
   resultDisplay: block;
   background-color: #ff9fa8;
 }
 
-#clear:hover {
+.mainCol #clear:hover {
   float: left;
   resultDisplay: block;
   background-color: #F297A0;
@@ -149,7 +190,7 @@ input:active {
   
 }
 
-#clear:active {
+.mainCol #clear:active {
   float: left;
   top: 4px;
   resultDisplay: block;
@@ -158,7 +199,7 @@ input:active {
   box-shadow: none;
 }
 
-    </style>
+</style>
   `;
 }
 
