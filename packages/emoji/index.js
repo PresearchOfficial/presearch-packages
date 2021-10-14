821d3b94cce3328d9aef451b79987f91307b8769
Ishan Ravindu <ishanravindu27596@gmail.com>
2021-10-14 17:44:43 +0530

    light version - responsive ui

--- packages/emoji/index.js
+++ packages/emoji/index.js
@@ -3,80 +3,62 @@ const { EmojiAPI } = require("emoji-api");
 const emoji_data = new EmojiAPI();
 
 async function emoji(query, API_KEY) {
-  let details = await emoji_data.get(query);
+  let emoji_details = await emoji_data.get(query);
 
   return `
-	<div id="presearchPackage">
-		<h1 class="mycolor">${details.name} - ${details.emoji}</h1>
-		<h2 class="mycolor">Unicode : ${details.unicode}</h2>
-		<p class="mycolor">Description : ${details.description}</p>
-<div class="imageContainer">
-		<div>
-			<img src=${details.images[0].url} alt="Girl in a jacket" width="50" height="60">
-			<figcaption>${details.images[0].vendor}</figcaption>
-		</div>
+<div id="presearchPackage">
+  <div class="container">
 
-		<div>
-			<img src=${details.images[1].url} alt="Girl in a jacket" width="50" height="60">
-			<figcaption>${details.images[1].vendor}</figcaption>
-		</div>
+  <div class="left-column">
+	  <div class="left-column-title">
+		  <h1>${emoji_details.emoji} ${emoji_details.name}</h1>	 
+	  </div>
+	  <div class="unicode-container">
+		  <h3 class="unicode-text">Unicode: ${emoji_details.unicode}</h3>		 
+	  </div>
+	  
+	  <div class="images-container">
+          <div class="image-container">
+            <img class="emoji-image" src=${emoji_details.images[4].url} alt=${emoji_details.name}  width="50" height="60">
+            <figcaption class="img-caption">${emoji_details.images[4].vendor}</figcaption>
+          </div>
+          <div class="image-container">
+            <img class="emoji-image" src=${emoji_details.images[6].url} alt=${emoji_details.name}  width="50" height="60">
+            <figcaption class="img-caption">${emoji_details.images[6].vendor}</figcaption>
+          </div>
+          <div class="image-container">
+            <img class="emoji-image" src=${emoji_details.images[5].url} alt=${emoji_details.name}  width="50" height="60">
+            <figcaption class="img-caption">${emoji_details.images[5].vendor}</figcaption>
+          </div>
+          <div class="image-container">
+            <img class="emoji-image" src=${emoji_details.images[0].url} alt=${emoji_details.name}  width="50" height="60">
+            <figcaption class="img-caption">${emoji_details.images[0].vendor}</figcaption>
+          </div>
+          <div class="image-container">
+            <img class="emoji-image" src=${emoji_details.images[2].url} alt=${emoji_details.name}  width="50" height="60">
+            <figcaption class="img-caption">${emoji_details.images[2].vendor}</figcaption>
+          </div>
+          <div class="image-container">
+            <img class="emoji-image" src=${emoji_details.images[3].url} alt=${emoji_details.name}  width="50" height="60">
+            <figcaption class="img-caption">${emoji_details.images[3].vendor}</figcaption>
+          </div>
+      </div>
+  </div>
 
-		<div>
-			<img src=${details.images[2].url} alt="Girl in a jacket" width="50" height="60">
-			<figcaption>${details.images[2].vendor}</figcaption>
-		</div>
+      <div class="right-column">
+        <div class="right-column-title">
+            <h2>Description</h2>
+            <hr/>
+            <br/>
+            <p>${emoji_details.description}</p>
+        </div>
+      </div>
 
-		<div>
-			<img src=${details.images[3].url} alt="Girl in a jacket" width="50" height="60">
-			<figcaption>${details.images[3].vendor}</figcaption>
-		</div>
+  </div>
+</div>
+<style>
 
-		<div>
-			<img src=${details.images[4].url} alt="Girl in a jacket" width="50" height="60">
-			<figcaption>${details.images[4].vendor}</figcaption>
-		</div>
-
-		<div>
-			<img src=${details.images[5].url} alt="Girl in a jacket" width="50" height="60">
-			<figcaption>${details.images[5].vendor}</figcaption>
-		</div>
-
-		<div>
-			<img src=${details.images[6].url} alt="Girl in a jacket" width="50" height="60">
-			<figcaption>${details.images[6].vendor}</figcaption>
-		</div>
-
-		<div>
-			<img src=${details.images[7].url} alt="Girl in a jacket" width="50" height="60">
-			<figcaption>${details.images[7].vendor}</figcaption>
-		</div>
-
-		<div>
-			<img src=${details.images[8].url} alt="Girl in a jacket" width="50" height="60">
-			<figcaption>${details.images[8].vendor}</figcaption>
-		</div>
-
-		<div>
-			<img src=${details.images[9].url} alt="Girl in a jacket" width="50" height="60">
-			<figcaption>${details.images[9].vendor}</figcaption>
-		</div>
-
-		<div>
-			<img src=${details.images[10].url} alt="Girl in a jacket" width="50" height="60">
-			<figcaption>${details.images[10].vendor}</figcaption>
-		</div>
-
-		<div>
-			<img src=${details.images[11].url} alt="Girl in a jacket" width="50" height="60">
-			<figcaption>${details.images[11].vendor}</figcaption>
-		</div>
-
-	</div>	
-	</div>
-	<!-- example styles - remember to use #presearchPackage before each class -->
-	<style>
-		/* styles for dark mode should have .dark before */
-		.dark #presearchPackage .mycolor {
+		/* .dark #presearchPackage .mycolor {
 			color: yellow;
 		}
 		#presearchPackage .mycolor {
@@ -85,7 +67,53 @@ async function emoji(query, API_KEY) {
 		}
 			#presearchPackage .imageContainer {
 			 display: flex;
-		}
+		} */
+	#presearchPackage	.container {
+  display: flex;
+  flex-wrap: wrap;
+}
+#presearchPackage .left-column {
+  padding: 10px;
+  flex: 50%;
+}
+#presearchPackage .right-column {
+  padding: 10px;
+  flex: 50%;
+}
+ #presearchPackage .image-container{
+  padding: 4px;
+  float: left;
+  width: 33.33%;
+  padding: 10px;
+}
+#presearchPackage .img-caption {
+  font-style: italic;
+  padding: 2px;
+  text-align: center;
+}
+#presearchPackage .emoji-image{
+    display: block;
+    margin-left: auto;
+    margin-right: auto
+}
+#presearchPackage .images-container{
+   padding:10px;
+}
+#presearchPackage .unicode-container{
+  padding:10px;
+}
+
+
+@media (max-width: 800px) {
+ #presearchPackage .right-column, .left-column {
+    flex: 100%;
+  }
+
+  @media screen and (max-width: 800px) {
+  #presearchPackage .image-container {
+    width: 50%;
+  }
+}
 	</style>
 	`;
 }
