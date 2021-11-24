22c4996fc9d0977c771e387dc9a5d39ebc26f294
Ishan-Ravindu <ishanravindu27596@gmail.com>
2021-11-24 15:28:48 +0530

    show suggest words in proper formate in the package card

--- packages/DidYouMean/index.js
+++ packages/DidYouMean/index.js
@@ -8,21 +8,69 @@ spell.load('en')
 
 async function DidYouMean(query, API_KEY) {
 
+	let suggestionListForQuery;
+
+	if (query && query.split(" ").length === 1) {
+		query = query ? query.toLowerCase() : "";
+		const check = spell.check(query)
+		if (check[0]) {
+			var arrayOfSuggestions = dictionary.suggest(check[0]);
+			if (arrayOfSuggestions.length > 0) {
+				suggestionListForQuery = arrayOfSuggestions
+			} else {
+				return null
+			}
+		} else {
+			return null
+		}
+	} else {
+		return null;
+	}
+
 	return `
 	<div id="presearchPackage">
-		<span class="myColor">Did You Mean</span>
+	<div class="container">
+		<span class="title">Did You Mean: </span>
+		${suggestionListForQuery && suggestionListForQuery.map((word, index) => (
+		`<span key="${index}" class="tag">${word}</span>`
+	)).join("")
+		}
+	</div>
 	</div>
 	<style>
-		.dark #presearchPackage .myColor {
-			color: yellow;
-		}
-		#presearchPackage .myColor {
-			color: green;
-			cursor: pointer;
-		}
+	#presearchPackage .container {
+		margin-bottom: 25px;
+	}
+	#presearchPackage .title {
+		color: red;
+	}
+	#presearchPackage .tag {
+		color: black;
+		padding:5px;
+		cursor: pointer;
+		border-style: solid;
+		border-color:rgba(243,244,246,var(--tw-bg-opacity));
+		border-width: 2px;
+		border-radius: 10px;
+		margin-right: 5px;
+  		margin-left: 5px;
+	}
+	.dark #presearchPackage .title {
+		color: red;
+	}
+	.dark #presearchPackage .tag {
+		color: rgba(243,244,246,var(--tw-bg-opacity));;
+		border-color:rgba(25,25,25,var(--tw-bg-opacity));
+	}
+	#presearchPackage .tag:hover {
+		background-color:  rgba(243,244,246,var(--tw-bg-opacity));
+	  }
+	.dark #presearchPackage .tag:hover {
+		background-color:  rgba(25,25,25,var(--tw-bg-opacity));
+	  }
 	</style>
 	<script>
-		document.querySelector(".myColor").addEventListener("click", () => alert("clicked!"));
+		
 	</script>
 	`;
 }
@@ -31,7 +79,6 @@ async function trigger(query) {
 	if (query && query.split(" ").length === 1) {
 		query = query ? query.toLowerCase() : "";
 		const check = spell.check(query)
-		console.log(check[0])
 		if (check[0]) {
 			var arrayOfSuggestions = dictionary.suggest(check[0]);
 			if (arrayOfSuggestions.length > 0) {
