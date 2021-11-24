8c048d47aebceb85bed8a4b00c9214e8199a1a15
Ishan-Ravindu <ishanravindu27596@gmail.com>
2021-11-24 14:28:17 +0530

    query handling for one wrong word

--- packages/DidYouMean/index.js
+++ packages/DidYouMean/index.js
@@ -1,4 +1,10 @@
 'use strict';
+var Typo = require("typo-js");
+const spell = require('spell-checker-js');
+const { identity } = require("mathjs");
+
+var dictionary = new Typo("en_US");
+spell.load('en')
 
 async function DidYouMean(query, API_KEY) {
 
@@ -22,9 +28,16 @@ async function DidYouMean(query, API_KEY) {
 }
 
 async function trigger(query) {
-	if (query) {
+	if (query && query.split(" ").length === 1) {
 		query = query ? query.toLowerCase() : "";
-		if (query === "did you mean") return true;
+		const check = spell.check(query)
+		console.log(check[0])
+		if (check[0]) {
+			var arrayOfSuggestions = dictionary.suggest(check[0]);
+			if (arrayOfSuggestions.length > 0) {
+				return true
+			}
+		}
 	}
 
 	return false;
