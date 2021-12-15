ddc27fa08794a089fa460f83af292cb2dbefd84e
Jejo <jejopl@o2.pl>
2021-12-15 13:52:34 +0100

    fixed text color in dark mode, changed author, disabe input listener when user is interacting with search input

--- packages/math/index.js
+++ packages/math/index.js
@@ -33,10 +33,10 @@ async function math(query) {
   </tr>
 
   <tr>
-    <td colspan="2">
+    <td colspan="2" style="padding-top:4px;">
       <button type="button" name="del" data-delete>DEL</button>
     </td>
-    <td colspan="2">
+    <td colspan="2" style="padding-top:4px;">
       <button type="button" name="ac" data-all-clear>AC</button>
     </td>
   </tr>
@@ -232,7 +232,17 @@ deleteButton.addEventListener('click', () => {
   calculator.updateDisplay()
 })
 
+let searchInputFocused = false;
+document.querySelector('input').addEventListener('click', e => {
+  searchInputFocused = true;
+})
+
+document.querySelector('#presearchPackage').addEventListener('mouseover', e => {
+  searchInputFocused = false;
+})
+
 document.addEventListener('keydown', function (event) {
+  if (searchInputFocused) return;
   event.preventDefault();
   switch (event.key) {
     case '+':
@@ -320,7 +330,7 @@ document.addEventListener('keydown', function (event) {
 #presearchPackage{
   
 }
-.output-container{
+#presearchPackage .output-container{
   width: 100%;
   height: 45px;
   text-align: right;
@@ -330,11 +340,16 @@ document.addEventListener('keydown', function (event) {
   border-style: solid;
   opacity: 0.5;
   margin: 100px;
+  padding-right:6px;
 }
-.output-container:hover{
+.dark #presearchPackage .output-container {
+  color: #fff;
+  opacity: .8;
+}
+#presearchPackage .output-container:hover{
   opacity: 1;
 }
-button[type=button]{
+#presearchPackage button[type=button]{
   opacity: 0.8;
   background-color: #f5f5f5;
   background-image: linear-gradient(top, #f5f5f5, #f1f1f1);
@@ -348,7 +363,7 @@ button[type=button]{
   font-size: 1.2em;
   font-weight: normal;
 }
-button[type=button]:hover{
+#presearchPackage button[type=button]:hover{
     box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
     background-image: linear-gradient(top, #f8f8f8, #f1f1f1);
     opacity: 1;
@@ -356,26 +371,26 @@ button[type=button]:hover{
     color: #222;
     cursor: pointer;
 }
-button[name=equal] {
+#presearchPackage button[name=equal] {
   background-color: #4d90fe;
   background-image: linear-gradient(top, #4d90fe, #4787ed);
   border: 1px solid #3079ed;
   color: #fefefe;
 }
-button[name=equal]:hover {
+#presearchPackage button[name=equal]:hover {
   background-color: #4d90fe;
   background-image: linear-gradient(top, #4d90fe, #357ae8);
   border: 1px solid #2f5bb7;
   color: #fefefe;
 }
-button[name=operator] {
+#presearchPackage button[name=operator] {
   border: 1px solid #c6c6c6;
   background-color: #d6d6d6;
 }
-button[name=ac] {
+#presearchPackage button[name=ac] {
   width: 100%;
 }
-button[name=del] {
+#presearchPackage button[name=del] {
   width: 100%;
 }
 </style>
