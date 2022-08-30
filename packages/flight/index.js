3653060a2a884b50b4819b24849548637a006915
Marijus Mackevic <m.mackevic@outlook.com>
2022-08-31 02:40:21 +0300

    Changed API provider to flightaware

--- packages/flight/index.js
+++ packages/flight/index.js
@@ -3,17 +3,20 @@
 const axios = require("axios");
 
 const dayjs = require("dayjs");
-const timezone = require('dayjs/plugin/timezone');
-const utc = require('dayjs/plugin/utc');
-const localizedFormat = require('dayjs/plugin/localizedFormat');
+const timezonePlugin = require('dayjs/plugin/timezone');
+const utcPlugin = require('dayjs/plugin/utc');
+const durationPlugin = require('dayjs/plugin/duration');
+const localizedFormatPlugin = require('dayjs/plugin/localizedFormat');
 
 const airports = require("./airports.json");
+const airlines = require("./airlines.json");
 
-dayjs.extend(utc);
-dayjs.extend(timezone);
-dayjs.extend(localizedFormat);
+dayjs.extend(utcPlugin);
+dayjs.extend(timezonePlugin);
+dayjs.extend(durationPlugin);
+dayjs.extend(localizedFormatPlugin);
 
-const FLIGHT_API_URL = "http://api.aviationstack.com/v1";
+const FLIGHT_API_URL = "https://aeroapi.flightaware.com/aeroapi";
 
 async function flight(query, API_KEY) {
     const data = await getFlight(query, API_KEY);
@@ -164,7 +167,7 @@ async function flight(query, API_KEY) {
                 });
             });
             
-            selectFlight(0);
+            selectFlight(data.selectedIndex);
         })();
     </script>
 
@@ -425,6 +428,92 @@ async function flight(query, API_KEY) {
     </style>`;
 }
 
+const toAirport = (flight, suffix, altsuffix) => {
+    const actualTime = flight['actual_' + suffix] || flight['actual_' + altsuffix];
+    const estimatedTime = flight['estimated_' + suffix] || flight['estimated_' + altsuffix];
+    const scheduledTime = flight['scheduled_' + suffix] || flight['scheduled_' + altsuffix];
+
+    const airportCode = (suffix === 'out' ? flight.origin : flight.destination).code_iata;
+    const delay = suffix === 'out' ? flight.departure_delay : flight.arrival_delay;
+    const terminal = suffix === 'out' ? flight.terminal_origin : flight.terminal_destination;
+    const gate = suffix === 'out' ? flight.gate_origin : flight.gate_destination;
+
+    const airport = airports[airportCode];
+
+    const dateTime = dayjs(actualTime || estimatedTime || scheduledTime).tz(airport.timezone);
+    const status = delay === 0 ? 'intime' : delay > 0 ? 'delayed' : 'early';
+
+    return {
+        code: airportCode,
+        date: dateTime.format('ddd, MMM D'),
+        city: airport.city_name,
+        terminal: terminal || '-',
+        gate: gate || '-',
+        time: dateTime.format('h:mm A'),
+        scheduledTime: dayjs(scheduledTime).tz(airport.timezone).format('h:mm A'),
+        status: status
+    }
+};
+
+const toAirline = (flight) => {
+    let airline = airlines[`${flight.operator_iata}:${flight.operator_icao}`];
+
+    if (!airline && flight.codeshares?.length !== 0) {
+        const airlineCodeRegex = /^[a-zA-Z]{1,3}/;
+
+        const codeshares = flight.codeshares.map((cs, index) => {
+            const icao = cs.match(airlineCodeRegex)[0];
+            const iata = (flight.codeshares_iata[index] || '').match(airlineCodeRegex)[0];
+            return airlines[`${iata}:${icao}`];
+        });
+
+        airline = codeshares.find(cs => !!cs);
+    }
+
+    return airline;
+};
+
+function toContract(flights) {
+    flights = flights.reverse();
+
+    const flightInfo = flights.find(x => dayjs(x.scheduled_out || x.scheduled_off).isSame(dayjs().utc(), 'day') ||
+                                        dayjs(x.scheduled_in || x.scheduled_on).isSame(dayjs().utc(), 'day')) ||
+                       flights.find(x => dayjs(x.scheduled_out || x.scheduled_off).isAfter(dayjs().utc(), 'day')) || 
+                       flights[0];
+
+    let airline = toAirline(flightInfo);
+
+    return {
+        airline: airline?.airline_name || flightInfo.operator_iata,
+        number: `${airline?.iata_code || flightInfo.operator_iata} ${flightInfo.flight_number}`,
+        selectedIndex: flights.indexOf(flightInfo),
+
+        flights: flights.map(f => {
+            const duration = dayjs.duration(f.filed_ete, 'seconds');
+            const durationHours = duration.hours();
+            const durationMins = duration.minutes();
+
+            const durationText = `${durationHours ? (durationHours + 'h') : ''} ${durationMins % 60}m`;
+
+            const status = f.blocked ? 'incident' :
+                f.diverted ? 'diverted' :
+                f.cancelled ? 'cancelled' :
+                f.progress_percent === 0 ? 'scheduled' :
+                f.progress_percent === 100 ? 'landed' :
+                f.progress_percent < 100 ? 'active' : 'unknown';
+
+            return {
+                date: dayjs(f.scheduled_out || f.scheduled_off).format('ddd, MMM D'),
+                status: status,
+                departure: toAirport(f, 'out'),
+                arrival: toAirport(f, 'in'),
+                duration: durationText,
+                progress: f.progress_percent
+            };
+        })
+    };
+}
+
 async function getFlight(query, API_KEY) {
     const flightNo = extractFlightNo(query);
 
@@ -433,77 +522,28 @@ async function getFlight(query, API_KEY) {
     }
 
     try {
-        const flightUrl = `${FLIGHT_API_URL}/flights`;
+        const flightUrl = `${FLIGHT_API_URL}/flights/${flightNo}`;
+        const dateFormat = 'YYYY-MM-DDTHH:mm:ss';
+
         const { data } = await axios.get(flightUrl, {
+            headers: {
+                'x-apikey': API_KEY
+            },
             params: {
-                "access_key": API_KEY,
-                "flight_iata": flightNo
+                start: dayjs().utc().startOf('day').subtract(1, 'day')
+                    .format(dateFormat) + 'Z',
+                end: dayjs().utc().endOf('day').add(1, 'day')
+                    .format(dateFormat) + 'Z',
             }
         });
 
-        const flights = data.data;
+        const flights = data.flights;
 
         if (!(flights?.length)) {
             return null;
         }
 
-        const flightInfo = flights[0];
-
-        const toAirportInfo = (airport) => {
-            const dateTime = dayjs.tz(airport.actual || airport.estimated || airport.scheduled, airport.timezone);
-            const isInTime = dateTime.utc().isSame(dayjs.tz(airport.estimated, airport.timezone).utc(), 'minute');
-            const isDelayed = dateTime.utc().isAfter(dayjs.tz(airport.estimated, airport.timezone).utc(), 'minute');
-            const status = isInTime ? 'intime' : isDelayed ? 'delayed' : 'early';
-
-            return {
-                code: airport.iata,
-                date: dateTime.format('ddd, MMM D'),
-                city: airports[airport.iata].city_name,
-                terminal: airport.terminal || '-',
-                gate: airport.gate || '-',
-                time: dateTime.format('h:mm A'),
-                scheduledTime: dayjs.tz(airport.scheduled, airport.timezone).format('h:mm A'),
-                status: status
-            }
-        };
-
-        return {
-            airline: flightInfo.airline.name,
-            number: `${flightInfo.airline.iata} ${flightInfo.flight.number}`,
-
-            flights: flights.map(f => {
-                const departureDate = dayjs.tz(f.departure.actual || f.departure.estimated, f.departure.timezone);
-                const arrivalDate = dayjs.tz(f.arrival.actual || f.arrival.estimated, f.arrival.timezone);
-
-                const durationHours = arrivalDate.diff(departureDate, 'h');
-                const durationMins = arrivalDate.diff(departureDate, 'm');
-
-                const duration = `${durationHours ? (durationHours + 'h') : ''} ${durationMins % 60}m`;
-
-                let progress = 0;
-
-                switch (f.flight_status) {
-                    case 'active':
-                        const inAirMins = dayjs.tz(dayjs(), f.departure.timezone).utc().diff(departureDate.utc(), 'm');
-                        console.log(inAirMins);
-                        progress = Math.round(inAirMins * 100 / durationMins);
-                        progress = progress > 100 ? 100 : progress;
-                        break;
-                    case 'landed':
-                        progress = 100;
-                        break;
-                }
-
-                return {
-                    date: dayjs(f.flight_date, 'YYYY-MM-DD').format('ddd, MMM D'),
-                    status: f.flight_status,
-                    departure: toAirportInfo(f.departure),
-                    arrival: toAirportInfo(f.arrival),
-                    duration: duration,
-                    progress: progress
-                };
-            }).reverse()
-        };
+        return toContract(flights);
     }
     catch (error) {
         console.error(error);
