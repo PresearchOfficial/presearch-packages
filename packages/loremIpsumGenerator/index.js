5982462a8df34cdf83fd1cd672107a5a39529fd9
Jejo <jejopl@o2.pl>
2022-09-02 16:28:00 +0200

    code cleanup -wip

--- packages/loremIpsumGenerator/index.js
+++ packages/loremIpsumGenerator/index.js
@@ -11,31 +11,28 @@ async function loremIpsumGenerator(query) {
   }
 
   return `
-    <head>
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-     </head>
     <div id="presearchPackage">
       <div id="packageContainer">
-          <div id="title" class="title"><a href="/loremIpsumGenerator?q=lorem+100">Lorem ipsum placeholder text has been created</a></div>
+          <div id="title" class="title"><a href="/search?q=lorem+100">Lorem ipsum placeholder text has been created</a></div>
           <div id="output"></div>
-          <span id="copy" class="material-symbols-outlined" title="Copy">content_copy</span>
+          <span id="copy" title="Copy">
+            <svg style="width:20px;" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M502.6 70.63l-61.25-61.25C435.4 3.371 427.2 0 418.7 0H255.1c-35.35 0-64 28.66-64 64l.0195 256C192 355.4 220.7 384 256 384h192c35.2 0 64-28.8 64-64V93.25C512 84.77 508.6 76.63 502.6 70.63zM464 320c0 8.836-7.164 16-16 16H255.1c-8.838 0-16-7.164-16-16L239.1 64.13c0-8.836 7.164-16 16-16h128L384 96c0 17.67 14.33 32 32 32h47.1V320zM272 448c0 8.836-7.164 16-16 16H63.1c-8.838 0-16-7.164-16-16L47.98 192.1c0-8.836 7.164-16 16-16H160V128H63.99c-35.35 0-64 28.65-64 64l.0098 256C.002 483.3 28.66 512 64 512h192c35.2 0 64-28.8 64-64v-32h-47.1L272 448z"/></svg>
+          </span>
       </div>
       <div class="disclaimer">
           <div class="disclaimer_example"><b>Examples</b></div>
-          <div class="disclaimer_sample"><a href="/loremIpsumGenerator?q=lorem+10">Lorem Ipsum 10</a> generates 10 words</div>
-          <div class="disclaimer_sample"><a href="/loremIpsumGenerator?q=lorem+ipsum+64">Lorem Ipsum 64</a> generates 64 words</div>
-          <div class="disclaimer_sample"><a href="/loremIpsumGenerator?q=ipsum+100">Lorem Ipsum 100</a> generates 100 words</div>
+          <div class="disclaimer_sample"><a href="/search?q=lorem+10">Lorem Ipsum 10</a> generates 10 words</div>
+          <div class="disclaimer_sample"><a href="/search?q=lorem+ipsum+64">Lorem Ipsum 64</a> generates 64 words</div>
+          <div class="disclaimer_sample"><a href="/search?q=ipsum+100">Lorem Ipsum 100</a> generates 100 words</div>
       </div>
     </div>
 
     <script>
-    function copyToClipboard(containerId) {
-        const range = document.createRange();
-        range.selectNode(document.getElementById(containerId));
-        window.getSelection().removeAllRanges();
-        window.getSelection().addRange(range);
-        document.execCommand("copy");
-        window.getSelection().removeAllRanges();
+    const copyTextToClipboard = (text) => {
+      navigator.clipboard.writeText(text).then(() => {
+      }, (err) => {
+        console.error('Async: Could not copy text: ', err);
+      });
     }
   	let isMobile = false;
   	if (window.innerWidth < 500) {
@@ -46,7 +43,7 @@ async function loremIpsumGenerator(query) {
     const copy = document.querySelector("#copy");
     output.textContent = "${generatedStr}";
     copy.addEventListener(isMobile ? "touchstart" : "mousedown", () => {
-      copyToClipboard("output");
+      copyTextToClipboard("${generatedStr}");
       let temp = output.textContent;
       output.textContent = "Copied!";
       setTimeout(() => {
