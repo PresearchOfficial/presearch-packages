bec9524ad2311acdd50f90ba9d54789d8f81b18f
Null7 <70843928+Sudo-null7@users.noreply.github.com>
2023-07-05 12:22:46 +0100

    Added User Agent package (#192)
    * Create useragents
    
    * Delete useragents
    
    * Add files via upload
    
    * improved trigger function
    
    ---------
    
    Co-authored-by: Jejo <jejopl@o2.pl>

---
+++ packages/userAgents/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+async function userAgents() {
+	return `
+	<div id="presearchPackage">
+		<span id="agentSpan">Your User Agent</span>
+		<br>
+		<code id="agent" >Welp this is embarrassing, failed to get user agent </code>
+	</div>
+	<!-- example styles - remember to use #presearchPackage before each class -->
+	<style>
+		.dark #presearchPackage #agentSpan {
+			color: white;
+		}
+		#presearchPackage #agentSpan {
+			color: black;
+		}
+		.dark #presearchPackage #agent {
+			color: white;
+		}
+		#presearchPackage #agent {
+			color: black;
+		}
+	</style>
+	<script>
+		let agent = navigator.userAgent;	
+		document.getElementById("agent").textContent = agent;
+	</script>
+	`;
+}
+async function trigger(query) {
+	if (query) {
+		query = query ? query.toLowerCase() : "";
+		if (query.includes("user agent") || query.includes("useragent")) return true;
+	}
+	return false;
+}
+
+module.exports = { userAgents, trigger };
