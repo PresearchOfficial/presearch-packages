d9fde3ab0b87b3e56cb3b3164e99031cf431e8c9
Mohd Shariq Khan <shariq@Mohds-MacBook-Air.local>
2023-07-13 17:31:23 +0530

    Adding from Language functionality

--- packages/translation/index.js
+++ packages/translation/index.js
@@ -1,9 +1,7 @@
 "use strict";
 
 const dotenv = require("dotenv");
-// const { SnetSDK, DefaultPaymentStrategy } = require("snet-sdk-shark");
 const SnetSDK = require("./lib/dist").default;
-//  const {SnetSDK} = require("./lib/src/sdk-core");
 const service = require("./translate_grpc_pb");
 const messages = require("./translate_pb");
 
@@ -13,28 +11,44 @@ function wordAfterLastTo(query) {
   return query.split("to")[query.split("to").length - 1].trim();
 }
 
+function getQueryLanguages(query) {
+	return query.split("from")[query.split("from").length - 1].trim();
+}
+function wordAfterLastFrom(query) {
+	return getQueryLanguages(query).split("to")[0].trim();
+  }
+  
 function extractQuery(query) {
-	const possibleLangWord = wordAfterLastTo(query);
-  const queryWithoutLang = query
-    .substr(10, query.length - possibleLangWord.length - 10)
-    .trim();
-  const queryWithoutLangAndTo = queryWithoutLang
-    .substr(0, queryWithoutLang.length - 2)
-    .trim();
-  return queryWithoutLangAndTo;
+	if( findMatchingLanguage(languages, wordAfterLastFrom(query)) ) {
+		const queryWithoutLang = query
+			.substr(10, query.length - getQueryLanguages(query).length - 10)
+			.trim();
+		const queryStatement = queryWithoutLang
+			.substr(0, queryWithoutLang.length - 4)
+			.trim();
+		return queryStatement;
+	} else {
+		const possibleLangWord = wordAfterLastTo(query);
+		const queryWithoutLang = query
+		  .substr(10, query.length - possibleLangWord.length - 10)
+		  .trim();
+		const queryWithoutLangAndTo = queryWithoutLang
+		  .substr(0, queryWithoutLang.length - 2)
+		  .trim();
+		return queryWithoutLangAndTo;	  
+	}
 }
 
 const config = {
   privateKey:
     "<your private key here>",
   networkId: 1,
-  org_id: "naint",
-  service_id: "machine-translation",
+  orgId: "naint",
+  serviceId: "machine-translation",
   web3Provider: "<your web3 provider here>",
 };
 
 function findMatchingLanguage(languageList, language) {
-  // languageList = [{language, langCode}]
   const matchingLanguage = languageList.filter(
     (lang) => lang.language.toLowerCase() === language.toLowerCase()
   )[0];
@@ -46,22 +60,23 @@ function findMatchingLanguage(languageList, language) {
 dotenv.config();
 const sdk = new SnetSDK(config);
 async function translation(query, API_KEY) {
-  const possibleLangWord = wordAfterLastTo(query);
-  const detectedLanguage = findMatchingLanguage(languages, possibleLangWord);
+	const possibleFromLang = wordAfterLastFrom(query);
+	const possibleToLang = wordAfterLastTo(query);
+	const detectedFromLanguage = findMatchingLanguage(languages, possibleFromLang);
+	const detectedToLanguage = findMatchingLanguage(languages, possibleToLang);
   const client = await sdk.createServiceClient(
-    "naint",
-    "machine-translation",
+    config.orgId,
+    config.serviceId,
     service.RomanceTranslatorClient,
     "default_group",
     null,
     { email: "dev.codesymphony@gmail.com", tokenToMakeFreeCall: "" }
   );
   const translateReq = new Promise(function (resolve, reject) {
-    // Uncomment this to test real translation
     const input = new messages.Input();
     input.setSentencesUrl(extractQuery(query));
-    input.setSourceLang("eng_Latn");
-    input.setTargetLang(detectedLanguage || "fra_Latn");
+    input.setSourceLang(detectedFromLanguage || "eng_Latn");
+    input.setTargetLang(detectedToLanguage || "fra_Latn");
     client.service.translate(input, (err, resp) => {
     	console.log(err);
     	console.log(resp);
@@ -70,12 +85,9 @@ async function translation(query, API_KEY) {
     	if (err) reject(err);
     	resolve(resp);
     });
-    // resolve({ array: ["translated mock text"] });
   });
   const resp = await translateReq;
   const translation = resp.array[0];
-  // here you need to return HTML code for your package. You can use <style> and <script> tags
-  // you need to keep <div id="presearchPackage"> here, you can remove everything else
   return `
 	<div id="presearchPackage">
 		<span class="mycolor">Translation: ${translation}</span>
@@ -83,20 +95,16 @@ async function translation(query, API_KEY) {
 	`;
 }
 
-//	here you should check, if the query should trigger your package
-//	ie. if you are building 'randomNumber' package, 'random number' query will be a good choice
 async function trigger(query) {
   if (query) {
-    // convert query to lower case, to trigger the package with queries like 'Random number', 'RANDOM NUMBER' etc.
     query = query ? query.toLowerCase() : "";
     if (
       query.indexOf("translate ") === 0 &&
       query.indexOf(" to ") > -1 &&
-      findMatchingLanguage(languages, wordAfterLastTo(query))
+	  findMatchingLanguage(languages, wordAfterLastTo(query))
     )
       return true;
   }
-  // you need to return false when the query should not trigger your package
   return false;
 }
 
